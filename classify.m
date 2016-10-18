28ee90ce570ddc66bf43279e44d0158c08578dd6
Thomas Loubiou <tloubiou@emi.u-bordeaux1.fr>
2016-10-18 08:41:52 +0200

    Bayesien à postériori

--- classify.m
+++ classify.m
@@ -1,6 +1,9 @@
 function [ resultat ] = classify( aClassifier, modeleClassifieur )
     probaC1 = normpdf(aClassifier, modeleClassifieur.mu1, modeleClassifieur.sigma1);
     probaC2 = normpdf(aClassifier, modeleClassifieur.mu2, modeleClassifieur.sigma2);
-    resultat = (probaC2 > probaC1) + 1;
+    
+    probaC1APosteriori = probaC1 .* modeleClassifieur.proba1 ./ (probaC1 + probaC2);
+    probaC2APosteriori = probaC2 .* modeleClassifieur.proba2 ./ (probaC1 + probaC2);
+    resultat = (probaC2APosteriori > probaC1APosteriori) + 1;
 end
 
--- trainClassifier.m
+++ trainClassifier.m
@@ -1,10 +1,10 @@
 function [ modeleClassifieur ] = trainClassifier( trainC1, trainC2 )
     modeleClassifieur.mu1 = mean(trainC1);
     modeleClassifieur.sigma1 = sqrt(var(trainC1));
+    modeleClassifieur.proba1 = size(trainC1, 1) / (size(trainC1, 1) + size(trainC2, 1));
     
     modeleClassifieur.mu2 = mean(trainC2);
     modeleClassifieur.sigma2 = sqrt(var(trainC2));
-    
-    descenteGradient(trainC1)
+    modeleClassifieur.proba2 = size(trainC2, 1) / (size(trainC1, 1) + size(trainC2, 1));
 end
 
