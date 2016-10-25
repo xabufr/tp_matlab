88aa2e3d79c51b7f53a082a13d5c1bcb494e8f50
Thomas Loubiou <tloubiou@emi.u-bordeaux1.fr>
2016-10-25 11:55:16 +0200

    Avancement CC exo 2 OK

--- apprentissage.m
+++ apprentissage.m
@@ -1,18 +1,14 @@
 % Écrire myclassifier
 % Écrire extractTrainTest
-muC2 = [12 12];
-sigmaC2 = [4 0.4];
-muC1 = [8 8];
-sigmaC1 = [1 0.1];
-sizeVT = 1000;
-VTC2 = mvnrnd(muC2,sigmaC2,	sizeVT);
-VTC1 = mvnrnd(muC1,sigmaC1,sizeVT);
+load DonneesCC.mat
 
 nbIter = 100;
-sizeTrain = 100;
-erreursC2 = [];
-erreursC1 = [];    
-
+erreursC1pourC1C2 = [];
+erreursC1pourC1C3 = [];
+erreursC2pourC1C2 = [];
+erreursC2pourC2C3 = [];
+erreursC3pourC1C3 = [];
+erreursC3pourC2C3 = [];
 %calculACI(VTC2, VTC1);
 % Calcul du PCA sur l'ensemble des échantillons
 %[vecteursPopres, VTC2Reduit, VTC1Reduit] = calculPCA(VTC2, VTC1);
@@ -24,21 +20,31 @@ erreursC1 = [];
 
 for	i=1:nbIter
     % Exctraction des jeux de tests
-    [testC1, trainC1] = extractTestAndTrain(VTC1, 10);
-    [testC2, trainC2] = extractTestAndTrain(VTC2, 10);
-    modeleClassifieur = trainClassifier(trainC1, trainC2);
+    [testC1, trainC1] = extractTestAndTrain(x1, 10);
+    [testC2, trainC2] = extractTestAndTrain(x2, 10);
+    [testC3, trainC3] = extractTestAndTrain(x3, 10);
+    modeleClassifieurC1C2 = trainClassifier(trainC1, trainC2);
+    modeleClassifieurC1C3 = trainClassifier(trainC1, trainC3);
+    modeleClassifieurC2C3 = trainClassifier(trainC2, trainC3);
 
     % Classification
-    ResC1 = classify(testC1,modeleClassifieur);
-    ResC2 = classify(testC2,modeleClassifieur);
-    % Calculs des taux d'erreur
-    nbC2Erreur = 2*size(ResC2,1) - sum(ResC2);
-    nbC1Erreur = sum(ResC1) - size(ResC1,1);
-    tauxC2 = (nbC2Erreur * 100 / size(testC2, 1));
-    tauxC1 = (nbC1Erreur * 100 / size(testC1, 1));
-    erreursC2 = [erreursC2 tauxC2];
-    erreursC1 = [erreursC1 tauxC1];
+    [tauxC1, tauxC2] = testClassifier(testC1, testC2, modeleClassifieurC1C2);    
+    erreursC2pourC1C2 = [erreursC2pourC1C2; tauxC2];
+    erreursC1pourC1C2 = [erreursC1pourC1C2; tauxC1];
+    [tauxC1, tauxC3] = testClassifier(testC1, testC3, modeleClassifieurC1C3);    
+    erreursC3pourC1C3 = [erreursC3pourC1C3; tauxC3];
+    erreursC1pourC1C3 = [erreursC1pourC1C3; tauxC1];
+    [tauxC2, tauxC3] = testClassifier(testC2, testC3, modeleClassifieurC2C3);    
+    erreursC2pourC2C3 = [erreursC2pourC2C3; tauxC2];
+    erreursC3pourC2C3 = [erreursC3pourC2C3; tauxC3];
 end;
 hold on;
-plot([1:nbIter], erreursC2, 'r');
-plot([1:nbIter], erreursC1, 'b');
+boites = [erreursC1pourC1C3 erreursC1pourC1C2 erreursC2pourC1C2 erreursC2pourC2C3 erreursC3pourC2C3 erreursC3pourC1C3 ];
+boxplot(boites, 'Labels', {'C1/C1C3','C1/C1C2','C2/C1C2','C2/C2C3','C3/C2C3','C3/C1C3'});
+
+% plot([1:nbIter], erreursC1pourC1C2, 'r');
+% plot([1:nbIter], erreursC1pourC1C3, '-dr');
+% plot([1:nbIter], erreursC2pourC1C2, 'g');
+% plot([1:nbIter], erreursC2pourC2C3, '-dg');
+% plot([1:nbIter], erreursC3pourC1C3, 'b');
+% plot([1:nbIter], erreursC3pourC2C3, '-db');
